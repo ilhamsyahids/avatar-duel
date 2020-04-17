c0b2fde741ffb244ad332dfa1c53f1598648f642
Ilham Syahid S <ilhamsyahids@gmail.com>
2020-04-17 14:27:54 +0700

    ke commit maaf

--- src/main/java/com/avatarduel/view/Untitled-1.py
+++
@@ -1,6 +0,0 @@
-x=[0]*10005;              #inisialisasi array 0 sebanyak 10005; x[0]=0
-x[1]=1;                   #x[1]=1
- 
-for j in range(2,10001):
-      x[j]=x[j-1]+x[j-2]  # Fibonacci
-print(x[10000])
